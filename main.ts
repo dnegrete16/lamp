f648b91f6cf00afe36628bcc83b6947b470a680d
dnegrete16 <dnegrete0006@mymail.lausd.net>
2025-05-15 11:45:07 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Light_Level_LDR = 0
+basic.forever(function () {
+    Light_Level_LDR = pins.analogReadPin(AnalogReadWritePin.P1)
+    if (Light_Level_LDR >= 1020) {
+        basic.showString("\"Wear cap\"")
+    } else {
+        if (Light_Level_LDR == 1019) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+            basic.pause(500)
+        } else {
+            if (Light_Level_LDR <= 1018) {
+                basic.showLeds(`
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    # . . . #
+                    . # # # .
+                    `)
+                basic.pause(500)
+            }
+        }
+    }
+})
